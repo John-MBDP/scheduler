5eaa83f8ac72259cd12ffa1190979629a7f6ff4a
John <johnartuz@hotmail.com>
2022-01-19 15:43:11 -0500

    Import classNames module.  Add interviewerEffect when interviewer is selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const { id, name, avatar, setInterviewer } = props;
+  const { id, name, avatar, selected, setInterviewer } = props;
   console.log(props);
+  const interviewerEffect = classNames("interviewers__item", { "interviewers__item--selected": selected });
 
   return (
     <li
       onClick={() => {
         setInterviewer(id);
       }}
-      className="interviewers__item"
+      className={interviewerEffect}
     >
       <img className="interviewers__item-image" src={avatar} alt="Sylvia Palmer" />
       {name}
