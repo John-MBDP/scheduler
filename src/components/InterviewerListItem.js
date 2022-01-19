b1235fcd27292b5ea003f6837dd0879b2857bbc8
John <johnartuz@hotmail.com>
2022-01-19 15:28:34 -0500

    Add new story for InterviewerListItem.  Create new CSS file for InterviewerListItem and import it to the component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,20 +1,11 @@
-import React from 'react';
+import React from "react";
+import "./InterviewerListItem.scss";
 
-
-function InterviewerListItem() {
+export default function InterviewerListItem() {
   return (
     <li className="interviewers__item">
-      <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
-      />
+      <img className="interviewers__item-image" src="https://i.imgur.com/LpaY82x.png" alt="Sylvia Palmer" />
       Sylvia Palmer
     </li>
   );
 }
-
-
-
-
-export default 
