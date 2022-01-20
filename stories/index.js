cc1a299299c4d526fe3854f77e8235729a8368a3
John <johnartuz@hotmail.com>
2022-01-19 21:05:43 -0500

    Add new Header story

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
 
 storiesOf("Button", module)
   .addParameters({
@@ -99,5 +100,4 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Appointment time="12pm" />)
-  .add("Empty", () => <Appointment time="12pm" />);
+  .add("Header", () => <Header time="12pm" />);
