1302e57042445b7bd51e2e59f80d6806998adb3c
John <johnartuz@hotmail.com>
2022-01-20 20:50:38 -0500

    Add getAppointmentsForDays functionality

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  let filteredDay = state.days.filter((d) => d.name === day)[0];
+  if (!filteredDay) {
+    return [];
+  }
+
+  let results = [];
+  if (filteredDay.appointments.length > 0) {
+    for (let id of filteredDay.appointments) {
+      results.push(state.appointments[id]);
+    }
+  }
+  return results;
+}
