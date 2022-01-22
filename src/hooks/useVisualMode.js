d3442c3b1cd813ca66bd5f1006c1616784d8abbf
John <johnartuz@hotmail.com>
2022-01-21 22:02:44 -0500

    Installed new package.  Create transition hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,46 @@
+// import { useState } from "react";
+
+// export default function useVisualMode(initial) {
+//   const [mode, setMode] = useState(initial);
+//   const [history, setHistory] = useState[[initial]];
+
+//   //transition to a new mode
+//   function transition(current, replace) {
+//     replace && back();
+//     let newHistory = [...history];
+//     newHistory.push(current);
+//     setHistory(newHistory);
+//     setMode(current);
+//   }
+
+//   function back() {
+//     let newHistory = [...history];
+//     newHistory.length > 1 && newHistory.pop() && setHistory(newHistory);
+//     setMode(() => newHistory[newHistory.length - 1]);
+//   }
+//   return { mode, transition, back };
+// }
+
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  //transition to a new mode
+  function transition(current, replace) {
+    replace && back();
+    let newHistory = [...history];
+    newHistory.push(current);
+    setHistory(newHistory);
+    setMode(current);
+  }
+
+  function back() {
+    let newHistory = [...history];
+    newHistory.pop();
+    setHistory(newHistory);
+    setMode(newHistory[newHistory.length - 1]);
+  }
+  return { mode, transition, back };
+}
